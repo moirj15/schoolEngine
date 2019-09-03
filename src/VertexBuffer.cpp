507157fab1d30d2115d03e3c07f4f515f49e4086
moirj15 <moirj15@gmail.com>
2019-09-02 20:46:28 -0400

    added vertex buffer implementation

---
+++ src/VertexBuffer.cpp
@@ -0,0 +1,80 @@
+#include "VertexBuffer.h"
+
+#include <GL/glew.h>
+
+
+VertexBuffer::VertexBuffer(const f32 const *vertecies, const size_t size, 
+        std::vector<BufferLayout> layout) :
+        m_vertexBufferHandle(0),
+        m_layout{std::move(layout)} {
+    glGenBuffers(1, &m_vertexBufferHandle);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferHandle);
+    glBufferData(GL_ARRAY_BUFFER, size * sizeof(f32), vertecies, GL_STATIC_DRAW);
+}
+
+VertexBuffer::~VertexBuffer() {
+    glDeleteBuffers(1, &m_vertexBufferHandle);
+}
+
+void VertexBuffer::Bind() {
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferHandle);
+}
+
+void VertexBuffer::Unbind() {
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+IndexBuffer::IndexBuffer(const u32 *indecies, const size_t size) :
+        m_indexBufferHandle(0),
+        m_count(size) {
+    glGenBuffers(1, &m_indexBufferHandle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBufferHandle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size * sizeof(u32), indecies, GL_STATIC_DRAW);
+}
+
+IndexBuffer::~IndexBuffer() {
+    glDeleteBuffers(1, &m_indexBufferHandle);
+}
+
+void IndexBuffer::Bind() {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBufferHandle);
+}
+
+void IndexBuffer::Unbind() {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+
+VertexArray::VertexArray() :
+        m_vertexArrayHandle(0) {
+    glGenVertexArrays(1, &m_vertexArrayHandle);
+}
+
+VertexArray::~VertexArray() {
+    glDeleteVertexArrays(1, &m_vertexArrayHandle);
+}
+
+void VertexArray::AddVertexBuffer(VertexBuffer *vertexBuffer) {
+    glBindVertexArray(m_vertexArrayHandle);
+    vertexBuffer->Bind();
+    for (const auto &layout : vertexBuffer->Layout()) {
+        glEnableVertexAttribArray(layout.index);
+        glVertexAttribPointer(layout.index, layout.elementSize, layout.type, GL_FALSE,
+            layout.stride, (void*)0);
+    }
+    m_vertexBuffers.emplace_back(vertexBuffer);
+}
+
+void VertexArray::AddIndexBuffer(IndexBuffer *indexBuffer) {
+    glBindVertexArray(m_vertexArrayHandle);
+    indexBuffer->Bind();
+    m_indexBuffer.reset(indexBuffer);
+}
+
+void VertexArray::Bind() {
+    glBindVertexArray(m_vertexArrayHandle);
+}
+
+void VertexArray::Unbind() {
+    glBindVertexArray(0);
+}
