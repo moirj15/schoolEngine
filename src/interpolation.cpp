0c01202e977fc94aca98fbe9014aaf5befb2ba22
moirj15 <moirj15@gmail.com>
2019-09-26 17:35:53 -0400

    Finished assignment, may not be completely correct

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -36,7 +36,6 @@ static inline glm::vec3 lerp(const f32 u, const glm::vec3 &p0, const glm::vec3 &
 
 glm::vec3 DCCSpline(const f32 u, const glm::vec3 &start, const glm::vec3 &p, 
         const glm::vec3 &end) {
-    printf("%f\n", u);
     const auto controlPoints = CreateControlPoints(start, p, end);
     const glm::vec3 q0 = lerp(u, controlPoints[0], controlPoints[1]);
     const glm::vec3 q1 = lerp(u, controlPoints[1], controlPoints[2]);
@@ -52,10 +51,10 @@ glm::quat DCCSpline(const f32 u, const KeyFrame &start, const KeyFrame &p, const
     glm::quat endQuat{glm::angleAxis(end.angle, end.axis)};
 
     const auto controlPoints = CreateControlPoints(startQuat, pQuat, endQuat);
-    const auto q0 = glm::slerp(controlPoints[0], controlPoints[1], u);
-    const auto q1 = glm::slerp(controlPoints[1], controlPoints[2], u);
-    const auto q2 = glm::slerp(controlPoints[2], controlPoints[3], u);
-    const auto r0 = glm::slerp(q0, q1, u);
-    const auto r1 = glm::slerp(q1, q2, u);
+    const auto q0 = glm::normalize(glm::slerp(controlPoints[0], controlPoints[1], u));
+    const auto q1 = glm::normalize(glm::slerp(controlPoints[1], controlPoints[2], u));
+    const auto q2 = glm::normalize(glm::slerp(controlPoints[2], controlPoints[3], u));
+    const auto r0 = glm::normalize(glm::slerp(q0, q1, u));
+    const auto r1 = glm::normalize(glm::slerp(q1, q2, u));
     return glm::normalize(glm::slerp(r0, r1, u)); 
 }
